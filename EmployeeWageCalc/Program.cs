507f4782892a8bd2dd543700b6771064def77c87
Shri10 <pethe.shriraj@gmail.com>
2023-05-09 16:26:02 +0530

    [Shri10] Created UC6

--- EmployeeWageCalc/Program.cs
+++ EmployeeWageCalc/Program.cs
@@ -10,15 +10,18 @@ namespace EmployeeWageCalc
             const int PARTTIME = 2;
             const int EMP_RATE_PER_HR = 20;
             const int MAX_WORKING_DAYS = 20;
+            const int MAX_WORKING_HOURS = 100;
             int empHrs = 0;
             int empWage = 0;
             int totalWage = 0;
+            int day = 1;
+            int totalWrkHrs = 0;
 
             Console.WriteLine("Welcome to Employee Wage Computation Program!");
 
             Random random = new Random();
 
-            for (int day = 0; day <= MAX_WORKING_DAYS; day++)
+            while ( day<= MAX_WORKING_DAYS && totalWrkHrs <= MAX_WORKING_HOURS)
             {
                 Console.WriteLine("\n\tDay {0}\n", day);
 
@@ -42,10 +45,12 @@ namespace EmployeeWageCalc
 
                 empWage = EMP_RATE_PER_HR * empHrs;
                 Console.WriteLine("Employee Wage for Day {0}: {1}", day, empWage);
+                day++;
                 totalWage += empWage;
+                totalWrkHrs += empHrs;
             }
 
-            Console.WriteLine("Employee Wage for All 20 Days: {0}", totalWage);
+            Console.WriteLine("\n\nEmployee Wage for {0} Days & {1} Hours is: {2}", day-1, totalWrkHrs, totalWage);
             Console.ReadLine();
 
         }
