6c400b887ce0f70772ae1fd601e35e4e17b13f0e
Shri10 <pethe.shriraj@gmail.com>
2023-05-09 10:50:28 +0530

    [Shri10] Created UC1

--- EmployeeWageCalc/Program.cs
+++ EmployeeWageCalc/Program.cs
@@ -6,7 +6,23 @@ namespace EmployeeWageCalc
     {
         static void Main(string[] args)
         {
+            const int IS_PRESENT = 1;
             Console.WriteLine("Welcome to Employee Wage Computation Program!");
+
+            Random random = new Random();
+            
+            int randomInput = random.Next(0, 2);
+
+            if (IS_PRESENT == randomInput)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+            Console.ReadLine();
+
         }
     }
 }
