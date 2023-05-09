b87537c001c36db26d6b40766296504677d255c8
Shri10 <pethe.shriraj@gmail.com>
2023-05-09 10:57:10 +0530

    [Shri10] Created UC2

--- EmployeeWageCalc/Program.cs
+++ EmployeeWageCalc/Program.cs
@@ -6,21 +6,30 @@ namespace EmployeeWageCalc
     {
         static void Main(string[] args)
         {
-            const int IS_PRESENT = 1;
+            const int IS_FULLTIME = 1;
+            const int EMP_RATE_PER_HR = 20;
+            int empHrs = 0;
+            int empWage = 0;
+
             Console.WriteLine("Welcome to Employee Wage Computation Program!");
 
             Random random = new Random();
             
             int randomInput = random.Next(0, 2);
 
-            if (IS_PRESENT == randomInput)
+            if (IS_FULLTIME == randomInput)
             {
+                empHrs = 8;
                 Console.WriteLine("Employee is present");
             }
             else
             {
                 Console.WriteLine("Employee is absent");
             }
+
+            empWage = EMP_RATE_PER_HR * empHrs;
+            Console.WriteLine("Employee Wage Per Day: {0}", empWage);
+
             Console.ReadLine();
 
         }
