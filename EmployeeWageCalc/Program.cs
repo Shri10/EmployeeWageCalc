57a75febe8c9aaf6bc537aae958e505f250367bf
Shri10 <pethe.shriraj@gmail.com>
2023-05-09 15:42:27 +0530

    [Shri10] Created UC4

--- EmployeeWageCalc/Program.cs
+++ EmployeeWageCalc/Program.cs
@@ -9,28 +9,30 @@ namespace EmployeeWageCalc
             const int FULLTIME = 1;
             const int PARTTIME = 2;
             const int EMP_RATE_PER_HR = 20;
+            const int MAX_WORKING_DAYS = 20;
             int empHrs = 0;
             int empWage = 0;
 
             Console.WriteLine("Welcome to Employee Wage Computation Program!");
 
             Random random = new Random();
-            
-            int randomInput = random.Next(0, 2);
 
-            if (FULLTIME == randomInput)
+            int randomInput = random.Next(0, 3);
+
+            switch (randomInput)
             {
-                empHrs = 8;
-                Console.WriteLine("Fulltime Employee is present");
-            }
-            else if (PARTTIME == randomInput)
-            {
-                empHrs = 4;
-                Console.WriteLine("Parttime Employee is present");
-            }
-            else
-            {
-                Console.WriteLine("Employee is absent");
+                case FULLTIME:
+                    empHrs = 8;
+                    Console.WriteLine("Fulltime Employee is present");
+                    break;
+                case PARTTIME:
+                    empHrs = 4;
+                    Console.WriteLine("Parttime Employee is present");
+                    break;
+                default:
+                    empHrs = 0;
+                    Console.WriteLine("Employee is absent");
+                    break;
             }
 
             empWage = EMP_RATE_PER_HR * empHrs;
