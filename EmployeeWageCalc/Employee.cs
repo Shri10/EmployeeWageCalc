7b8e7f425bea58170aa5f87d4c58be25277d389b
Shri10 <pethe.shriraj@gmail.com>
2023-05-28 22:12:08 +0530

    [Shri10] Merged UC8

--- EmployeeWageCalc/Employee.cs
+++ EmployeeWageCalc/Employee.cs
@@ -6,49 +6,43 @@ namespace EmployeeWageCalc
 {
     internal class Employee
     {
-        const int FULLTIME = 1;
-        const int PARTTIME = 2;
-        const int EMP_RATE_PER_HR = 20;
-        const int MAX_WORKING_DAYS = 20;
-        const int MAX_WORKING_HOURS = 100;
+        public const int FULLTIME = 1;
+        public const int PARTTIME = 2;
 
-        public int ComputeWage()
+        public void ComputeEmpWageForCompany(string companyName, int EMP_RATE_PER_HR, int MAX_WORKING_DAYS, int MAX_WORKING_HOURS)
         {
             int empHrs = 0;
             int empWage = 0;
             int totalWage = 0;
-            int day = 1;
             int totalWrkHrs = 0;
+            int day = 1;
+
             Random random = new Random();
 
             while (day <= MAX_WORKING_DAYS && totalWrkHrs <= MAX_WORKING_HOURS)
             {
-                Console.WriteLine("\n\tDay {0}\n", day);
                 int randomInput = random.Next(0, 3);
+
                 switch (randomInput)
                 {
                     case FULLTIME:
                         empHrs = 8;
-                        Console.WriteLine("Fulltime Employee is present");
                         break;
                     case PARTTIME:
                         empHrs = 4;
-                        Console.WriteLine("Parttime Employee is present");
                         break;
                     default:
                         empHrs = 0;
-                        Console.WriteLine("Employee is Absent");
                         break;
                 }
 
                 empWage = EMP_RATE_PER_HR * empHrs;
-                Console.WriteLine("Employee Wage for Day {0}: {1}", day, empWage);
-                day++;
                 totalWage += empWage;
                 totalWrkHrs += empHrs;
+                day++;
             }
-            return totalWage;
-        }
 
+            Console.WriteLine($"Total Employee Wage for {companyName} is {totalWage}");
+        }
     }
 }
--- EmployeeWageCalc/Program.cs
+++ EmployeeWageCalc/Program.cs
@@ -8,11 +8,40 @@ namespace EmployeeWageCalc
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program!");
 
+            //Employee employee  = new Employee();
+            /* Syntax Refernce
+             * ComputeEmpWageForCompany(string companyName, int EMP_RATE_PER_HR, int MAX_WORKING_DAYS, int MAX_WORKING_HOURS)
+             */
+            /*
+                        employee.ComputeEmpWageForCompany("BridgeLabz", 20, 20, 100);
+                        employee.ComputeEmpWageForCompany("Micros", 25, 25, 120);
+            */
+            //var employee = new Employee();
             Employee employee = new Employee();
-            int totalWage = employee.ComputeWage();
-            Console.WriteLine("\n\tEmployee Wage is: "+ totalWage);
 
-            Console.ReadLine();
+            string userInput;
+            do
+            {
+                Console.WriteLine("Enter Company Name: ");
+                string companyName = Console.ReadLine();
+
+                Console.WriteLine("Enter Employee Rate per Hour: ");
+                int empRatePerHr = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter Maximum Working Days: ");
+                int maxWorkingDays = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter Maximum Working Hours: ");
+                int maxWorkingHrs = Convert.ToInt32(Console.ReadLine());
+
+                employee.ComputeEmpWageForCompany(companyName, empRatePerHr, maxWorkingDays, maxWorkingHrs);
+
+                Console.WriteLine("\n\tDo you want to enter another company details? (yes/no)");
+                userInput = Console.ReadLine();
+            }
+            while (userInput.ToLower() == "yes");
+
+            //Console.ReadLine();
         }
     }
 }
