cf10e0a5d020193705d6627097a250fcdd532f5e
Shri10 <pethe.shriraj@gmail.com>
2023-05-29 06:08:17 +0530

    [Shri10] ReCommit

---
+++ EmployeeWageCalc/CompanyEmpWage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageCalc
+{
+    internal class CompanyEmpWage
+    {
+        public string CompanyName { get; set; }
+        public int EmpRatePerHr { get; set; }
+        public int MaxWorkingDays { get; set; }
+        public int MaxWorkingHrs { get; set; }
+        public int TotalWage { get; set; }
+
+        public CompanyEmpWage(string companyName, int empRatePerHr, int maxWorkingDays, int maxWorkingHrs)
+        {
+            CompanyName = companyName;
+            EmpRatePerHr = empRatePerHr;
+            MaxWorkingDays = maxWorkingDays;
+            MaxWorkingHrs = maxWorkingHrs;
+            TotalWage = 0;
+        }
+
+        public void SetTotalWage(int totalWage)
+        {
+            TotalWage = totalWage;
+        }
+
+        public string ToString()
+        {
+            return $"Total Employee Wage for {CompanyName} is {TotalWage}";
+        }
+
+    }
+}
---
+++ EmployeeWageCalc/EmpWageBuilder.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageCalc
+{
+    internal class EmpWageBuilder
+    {
+        public const int FULLTIME = 1;
+        public const int PARTTIME = 2;
+
+        private List<CompanyEmpWage> companyEmpWages;
+
+        public EmpWageBuilder()
+        {
+            companyEmpWages = new List<CompanyEmpWage>();
+        }
+
+        public void AddCompanyEmpWage(string companyName, int empRatePerHr, int maxWorkingDays, int maxWorkingHrs)
+        {
+            CompanyEmpWage companyEmpWage = new CompanyEmpWage(companyName, empRatePerHr, maxWorkingDays, maxWorkingHrs);
+            companyEmpWages.Add(companyEmpWage);
+        }
+
+        public void ComputeEmpWage()
+        {
+            foreach (CompanyEmpWage companyEmpWage in companyEmpWages)
+            {
+                companyEmpWage.SetTotalWage(this.ComputeEmpWageForCompany(companyEmpWage));
+                Console.WriteLine(companyEmpWage.ToString());
+            }
+        }
+
+        //public void ComputeEmpWageForCompany(Company company)
+        public int ComputeEmpWageForCompany(Company company)
+        {
+            int empHrs = 0;
+            int empWage = 0;
+            int totalWage = 0;
+            int totalWrkHrs = 0;
+            int day = 1;
+
+            Random random = new Random();
+
+            while (day <= company.MaxWorkingDays && totalWrkHrs <= company.MaxWorkingHours)
+            {
+                int randomInput = random.Next(0, 3);
+
+                switch (randomInput)
+                {
+                    case FULLTIME:
+                        empHrs = 8;
+                        break;
+                    case PARTTIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+
+                empWage = company.EmpRatePerHour * empHrs;
+                totalWage += empWage;
+                totalWrkHrs += empHrs;
+                day++;
+            }
+
+            company.TotalWage = totalWage;
+            return totalWage;
+        }
+    }
+}
